cd6a453811846f55052e802d6e8dabfaef69eb0c
Jakub Paluch <qbusik@gmail.com>
2026-01-08 08:37:10 +0100

    Flake8 changes

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -15,9 +15,18 @@ class FormsTests(TestCase):
         }
         form = DriverCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data["license_number"], form_data["license_number"])
-        self.assertEqual(form.cleaned_data["first_name"], form_data["first_name"])
-        self.assertEqual(form.cleaned_data["last_name"], form_data["last_name"])
+        self.assertEqual(
+            form.cleaned_data["license_number"],
+            form_data["license_number"]
+        )
+        self.assertEqual(
+            form.cleaned_data["first_name"],
+            form_data["first_name"]
+        )
+        self.assertEqual(
+            form.cleaned_data["last_name"],
+            form_data["last_name"]
+        )
 
         form_data = {
             "username": "test_tester1",
